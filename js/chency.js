3ee8b3d680089696855be0a6b2963b7a899b8988
chocolate <chenzy@blueuninfo.com>
2020-12-04 13:26:06 +0800

    Site updated: 2020-12-04 13:26:02

--- js/chency.js
+++ js/chency.js
@@ -27,14 +27,14 @@ function browserRedirect() {
 }
 
 
-/* 手机客户端导航栏默认隐藏 */
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " closed";
-}
+// /* 手机客户端导航栏默认隐藏 */
+// var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+// var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
+// var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
+// for (var i = 0; i < menus_item_child.length; i++) {
+//     menus_item_child[i].style.display = "none";
+//     menus_expand[i].className += " closed";
+// }
 
 // 可爱的Title
 var OriginTitle = document.title;
