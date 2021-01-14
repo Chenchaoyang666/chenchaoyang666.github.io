0b1effd04daebfd4d6480bccc827247e40d077e6
chocolate <1837364658@qq.com>
2021-01-14 23:20:30 +0800

    Site updated: 2021-01-14 23:20:13

--- js/chency.js
+++ js/chency.js
@@ -5,26 +5,26 @@
 
 /*添加图片top*/
 // 判断移动端设备
-browserRedirect();
+// browserRedirect();
 
-function browserRedirect() {
-    var sUserAgent = navigator.userAgent.toLowerCase();
-    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-    var bIsAndroid = sUserAgent.match(/android/i) == "android";
-    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
-    if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM)) {
-        var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-80px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
-        /*添加到返回顶部按钮下*/
-        document.getElementById("go-up").innerHTML += top_up;
-        document.getElementById("go-up").style.backgroundColor="transparent";
-    }
+// function browserRedirect() {
+//     var sUserAgent = navigator.userAgent.toLowerCase();
+//     var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+//     var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+//     var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+//     var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+//     var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+//     var bIsAndroid = sUserAgent.match(/android/i) == "android";
+//     var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+//     var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+//     if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM)) {
+//         var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-80px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+//         /*添加到返回顶部按钮下*/
+//         document.getElementById("go-up").innerHTML += top_up;
+//         document.getElementById("go-up").style.backgroundColor="transparent";
+//     }
     
-}
+// }
 
 
 // /* 手机客户端导航栏默认隐藏 */
